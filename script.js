f73afe468930df27efb6e301edd0c0cccd656174
Ayan Maji <88241719+theayan44@users.noreply.github.com>
2023-11-21 18:13:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this, make, model);
+	this.topSpeed = topSpeed;
+}
+
+Car.prototype.getMakeModel = function () {
+	return this.make + this.model;
+}
+
+SportsCar.prototype.getTopSpeed = function () {
+	return this.topSpeed;
+}
+
+SportsCar.prototype.__proto__ = Car.prototype;
 
 // Do not change the code below
 window.Car = Car;
